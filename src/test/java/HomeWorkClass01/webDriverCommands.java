a282345a0ed89391329faa3e281ecc5667451ae0
nfumba <fikile.fumba@gmail.com>
2024-09-20 21:26:40 +0200

    SeleniumHomeWork 2

--- src/test/java/HomeWorkClass01/webDriverCommands.java
+++ src/test/java/HomeWorkClass01/webDriverCommands.java
@@ -1,8 +1,6 @@
 package HomeWorkClass01;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class webDriverCommands {
