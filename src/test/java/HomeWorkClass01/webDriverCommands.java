fb8f1485dcad7eb2fe791b5980ffe3fa4a023989
nfumba <fikile.fumba@gmail.com>
2024-09-20 20:23:02 +0200

    SeleniumHomeWork 2

---
+++ src/test/java/HomeWorkClass01/webDriverCommands.java
@@ -0,0 +1,32 @@
+package HomeWorkClass01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class webDriverCommands {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://www.google.com");
+
+        String pagetitle = driver.getTitle();
+        System.out.println("page title is :" + pagetitle);
+
+        driver.navigate().to("https://www.syntaxprojects.com");
+
+        String title = driver.getTitle();
+        System.out.println("page title is :" + title);
+        Thread.sleep(2000);
+
+        driver.navigate().back();
+        Thread.sleep(2000);
+
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+
+driver.close();
+    }
+}
