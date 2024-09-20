dc3ff449b666865d6b9d6e87dc4900aa32315926
nfumba <fikile.fumba@gmail.com>
2024-09-20 20:23:02 +0200

    SeleniumHomeWork 1

---
+++ src/test/java/HomeWorkClass01/browserAssignment.java
@@ -0,0 +1,24 @@
+package HomeWorkClass01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class browserAssignment {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.syntaxprojects.com");
+        driver.manage().window().maximize();
+
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("the current url is :" + currentUrl);
+
+        String title = driver.getTitle();
+        System.out.println("the current title of the page is :" + title);
+
+        driver.close();
+    }
+
+}
