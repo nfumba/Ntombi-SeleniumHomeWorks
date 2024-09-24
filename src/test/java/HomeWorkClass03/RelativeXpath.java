621d71c2b7b81635fd95fbb83d93c8427cd8f375
nfumba <fikile.fumba@gmail.com>
2024-09-24 06:05:37 +0200

    SeleniumHomeWork 4

--- src/test/java/HomeWorkClass03/RelativeXpath.java
+++ src/test/java/HomeWorkClass03/RelativeXpath.java
@@ -5,18 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class RelativeXpath {
-    public static void main(String[] args) {
+import java.nio.file.WatchEvent;
 
-    WebDriver driver=new ChromeDriver();
+public class RelativeXpath {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
 //
         driver.manage().window().maximize();
         driver.get("https://syntaxprojects.com/Xpath-homework.php");
 
-    WebElement hobbies = driver.findElement(By.xpath("//input[@id='yourHobbiesId']"));
+        WebElement hobbies = driver.findElement(By.xpath("//input[@id='yourHobbiesId']"));
         hobbies.sendKeys("Movies");
 
-    WebElement button = driver.findElement(By.xpath("//button[text()='Click Here']"));
+        WebElement button = driver.findElement(By.xpath("//button[text()='Click Here']"));
         button.click();
 
         WebElement favmovies = driver.findElement(By.name("favoriteMovies"));
@@ -27,9 +29,40 @@ public class RelativeXpath {
         System.out.println(textelement.getText());
 
         WebElement city = driver.findElement(By.name("yourCity"));
-                city.sendKeys("Johannesburg");
+        city.sendKeys("Johannesburg");
+
+        // 6. Enter email addresses using indexing technique
+        WebElement email1 = driver.findElement(By.xpath("(//input[@name='PrivateEmail'])"));
+        email1.sendKeys("test1@example.com");
+        WebElement email2 = driver.findElement(By.xpath("(//input[@name='OfficeEmail'])"));
+        email2.sendKeys("test2@example.com");
+        WebElement email3 = driver.findElement(By.xpath("(//input[@name='ProfessionalEmail'])"));
+        email3.sendKeys("test3@example.com");
+
+        Thread.sleep(3000);
+
+        // 7. Enter client name and ID using operators technique
+        WebElement clientName = driver.findElement(By.xpath("//input[@name='clientName' and @type='text']"));
+        clientName.sendKeys("John Doe");
+        WebElement clientID = driver.findElement(By.xpath("//input[@name='clientId' or @type='number']"));
+        clientID.sendKeys("12345");
+
+        Thread.sleep(3000);
+
+        // 8. Enter street number and house number using any XPath technique
+        WebElement addressline1 = driver.findElement(By.xpath("//input[@name='StreetNo']"));
+        addressline1.sendKeys("123 Main St");
+        WebElement adressLine2 = driver.findElement(By.xpath("//input[@name='HouseNo']"));
+        adressLine2.sendKeys("456");
+
+        Thread.sleep(3000);
+        driver.quit();
 
 
-}
+
+        driver.quit();
+
+
+    }
 }
 
